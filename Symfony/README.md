3dd2691bed0e20934c38b7b340419d029267f761
root <root@shamini-HP-Pavilion-dv6-Notebook-PC.(none)>
2015-10-10 22:45:11 +0200

    Add symfony folder

---
+++ Symfony/README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on October 10, 2015, 10:03 pm.
---
+++ Symfony/src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+ /**
+     * @Route("/api/lucky/number")
+     */
+ 
+public function apiNumberAction()
+    {
+        $data = array(
+            'lucky_number' => rand(0, 100),
+        );
+
+        return new Response(
+            json_encode($data),
+            200,
+            array('Content-Type' => 'application/json')
+        );
+    }
+}
